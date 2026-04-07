cef4446a22c347944af61b1fd2ce196471392b6c
kamarveetil869 <kamar.veetil@gmail.com>
2026-04-08 00:17:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ async function getEthosToken() {
     return token;
   } catch (err) {
     console.error("❌ Ethos auth failed:", err.response?.data || err.message );
-        console.error(" Ethos auth url:",  process.env.ETHOS_API_KEY);
-      console.error(" Ethos auth key:",  process.env.ETHOS_BASE_URl);
+        console.error(" Ethos auth key:",  process.env.ETHOS_API_KEY);
+      console.error(" Ethos auth url:",  process.env.ETHOS_BASE_URl);
       console.error("response.data:" ,  response);
     return null;
   }
